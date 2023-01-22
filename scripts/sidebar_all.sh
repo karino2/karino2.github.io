2aca3f053c98e6f13ecb0765633dc80fcfabc7ab
Kazuma Arino <hogeika2@gmail.com>
2023-01-22 09:32:00 +0900

    Update sidebar_all.sh

--- scripts/sidebar_all.sh
+++ scripts/sidebar_all.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 pushd gen_sidebar; npm run conv ~/GoogleDriveMirror/DriveText/TextTL/; popd
-pushd ../_includes; git add --all; git commit -m "update hitokoto"; git push; popd
+git add ../_includes/mysidebar.html; git commit -m "update hitokoto"; git push
